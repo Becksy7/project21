1af8b0652f2e506216d5aef4e437f4eb83e4edfb
Tanya Ten <ori.tanya.ten@gmail.com>
2016-03-31 23:02:42 +0300

    new page about

--- build/js/main.js
+++ build/js/main.js
@@ -125,23 +125,28 @@ $(function() {
         })()
         ,Player = (function(){
             var fitContainerHeight = function(){
-                var $mainContent = $('.layout-main');
+                var $mainContent = $('.layout-main'),
+                    ph = $(window).height() - 70,
+                    ww = $(window).width();
                 $mainContent.find('.container').css('height','auto');
-                if ($mainContent.length){
-                    var ch = $mainContent.height(),
-                        cnth = $mainContent.find('.container').outerHeight();
-                    if (ch > cnth){
-                        $mainContent.find('.container').css('height',ch);
-                    } else {
-                        $mainContent.find('.container').css('height','auto');
+                if ($mainContent.length) {
+                    if ($('body').hasClass('page-about')) {
+                        //$mainContent.find('.fullscreen-section').removeAttr('style');
+                        if (ww > 1023) {
+
+                            $mainContent.find('.fullscreen-section:not(.custom-height)').css('height', ph);
+
+                        } else {
+                            $mainContent.find('.fullscreen-section:not(.custom-height)').removeAttr('style');
+                        }
                     }
-                }
-                if ($mainContent.hasClass('.page-about')){
-                    if ($('.about-img-wrap').css('display') == "block"){
-                        var h = $('.about-img-wrap').css('height') + 70;
-                        $(".player").css('height', h);
+
+                    var ch = $mainContent.height();
+                    cnth = $mainContent.find('.container').outerHeight();
+                    if (ch > cnth) {
+                        $mainContent.find('.container').css('height', ch);
                     } else {
-                        $(".player").css('height', "100%");
+                        $mainContent.find('.container').css('height', 'auto');
                     }
                 }
             };
